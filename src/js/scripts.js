b462ab10b6d4c773ee8274ed6e1c74152aff9a8e
Yegor Zaporozhets <egorserg4@gmail.com>
2018-11-19 01:33:00 +0300

    Add script for select color of tshirt.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,9 @@
-// Your code here...
+function tshirtColor(color) {
+  let currentBlock = document.querySelector('.color-block_current');
+  let picture = document.getElementById('getIt');
+  let clickedColorBlock = document.querySelector('.color-block_' + color);
+  picture.setAttribute('src',"img/tshirts/tshirt_" + color + ".jpg" );
+  currentBlock.classList.remove('color-block_current');
+  clickedColorBlock.classList.add('color-block_current');
+
+}
